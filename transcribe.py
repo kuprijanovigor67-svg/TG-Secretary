ba2319c3592c033f2addfb2be550ddcdadbbe350
user <user@tg-secretary.local>
2026-06-01 19:29:28 +0300

    Switch whisper model from turbo to tiny (save disk & RAM)

--- transcribe.py
+++ transcribe.py
@@ -5,6 +5,6 @@ sys.stdout.reconfigure(encoding='utf-8')
 if platform.system() == 'Windows':
     os.environ['PATH'] += r';C:\Program Files\KMPlayer 64X\LAVFilters64'
 
-model = whisper.load_model('turbo')
+model = whisper.load_model('tiny')
 result = model.transcribe(sys.argv[1], language='ru', task='transcribe')
 sys.stdout.write(result['text'] + '\n')
